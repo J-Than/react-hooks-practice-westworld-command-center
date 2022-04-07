bdaacdcf8b53d1af9eb45a02ed4d344b10dbd028
Jarod Wilson <jaroddwilson@gmail.com>
2022-04-06 20:31:13 -0400

    Completes checkpoint 6

--- src/components/Headquarters.js
+++ src/components/Headquarters.js
@@ -6,13 +6,24 @@ import LogPanel from "./LogPanel";
 import "../stylesheets/Headquarters.css";
 
 function Headquarters({ areas, hosts, selectedHost, onSelectHost, onActivation, onAreaChange }) {
+
+  function areaCheck(area) {
+    const hostsInArea = hosts.filter(h => h.area===area).length;
+    const areaToCheck = areas.find(a => a.name===area);
+    if (hostsInArea < areaToCheck.limit) {
+      onAreaChange(area);
+    } else {
+      console.log("ERROR: Too many hosts.")
+    }
+  }
+
   return (
     <Grid celled="internally">
       <Grid.Column width={8}>
         <ColdStorage hosts={hosts} selectedHost={selectedHost} onSelectHost={onSelectHost} />
       </Grid.Column>
       <Grid.Column width={5}>
-        <Details areas={areas} selectedHost={selectedHost} onActivation={onActivation} onAreaChange={onAreaChange} />
+        <Details areas={areas} selectedHost={selectedHost} onActivation={onActivation} onAreaChange={areaCheck} />
       </Grid.Column>
       <Grid.Column width={3}>
         <LogPanel />
